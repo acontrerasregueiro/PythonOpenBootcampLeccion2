19c5cea24dcd1a9b0c24c7a3445979dbdb8dec48
Adrian <acontrerasregueiro@gmail.com>
2022-08-17 10:59:53 +0200

    ejercicio tipos de datos Python OpenBootcamp Leccion 2

---
+++ app.py
@@ -0,0 +1,25 @@
+nombre = "jose"
+apellidos = "perez perez"
+edad = 33
+email = "joseperez@perez.com"
+telefono = 91555555
+casado = False
+hijos = True
+amigos = ['jose','laura','pedrito']
+peliculas = {
+    'titulo1' : 'Lo que el viento se llevo',
+    'titulo2' : 'El silencio de los corderos',
+    'titulo3' : 'Snach cerdos y diamantes'
+}
+
+print(nombre)
+print(apellidos)
+print(edad)
+print(email)
+print(telefono)
+print(casado)
+print(hijos)
+print(amigos)
+print(peliculas['titulo1'])
+print(peliculas['titulo2'])
+print(peliculas['titulo3'])
